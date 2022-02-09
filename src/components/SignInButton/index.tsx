c951ac82f23b091aa178a34bbb6ed72157a5fdf9
Leandro Araujo <leandroaraujo5511@gmail.com>
2022-02-09 18:46:29 -0300

    Criando autenticação com GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,27 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export function SignInButton() {
-    const isUserLoggedIn =  true;
+    const { data: session } = useSession()
     
-    return isUserLoggedIn ? (
+    console.log(session)
+    return session ? (
         <button 
             className={styles.signInButton}
             type="button"
-            
+            onClick={()=> signOut()}
         >
             <FaGithub color='#04d361'/>
-            Leandro Araújo
+            {session.user.name}
             <FiX color='#737380' className={styles.closseIcon}/>
         </button>
     ):(
         <button 
             className={styles.signInButton}
             type="button"
-            
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417'/>
             Sign in with GitHub
