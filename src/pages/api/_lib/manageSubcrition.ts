52ce8f0c6d740b6ca5842ef36e7aad28baccfb5c
Leandro Araujo <leandroaraujo5511@gmail.com>
2022-02-19 17:22:41 -0300

    Ouvindo eventos enviado do Stripe/FaunaDB

--- src/pages/api/_lib/manageSubcrition.ts
+++ src/pages/api/_lib/manageSubcrition.ts
@@ -1,7 +1,13 @@
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
-export async function saveSubscription(subscriptionId:string, customerId: string) {
+
+
+export async function saveSubscription(
+        subscriptionId:string,
+        customerId: string,
+        cretadAction =  false,
+    ) {
     //Buscar o user no banco do fauna com id customer id
     
     const userRef = await fauna.query(
@@ -18,12 +24,39 @@ export async function saveSubscription(subscriptionId:string, customerId: string
 
     const subscription =  await stripe.subscriptions.retrieve(subscriptionId)
 
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            {data: subscription}
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id
+        
+    }
+
+    if(cretadAction){
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                {data: subscriptionData}
+            )
         )
-    )
+
+    }else{
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId,
+                        )
+                    )
+                ),
+                {data: subscriptionData}
+            )
+        )
+    }
+
 
 
     //Salva dos dados da subscrition no falna DB
